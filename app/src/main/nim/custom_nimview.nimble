787fed8f60dc1d029810d5c1938e91fae49be563
Marco Mengelkoch <Mmengelkoch@gmx.de>
2021-03-14 11:36:23 +0100

    improved build speed by using nake

--- app/src/main/nim/custom_nimview.nimble
+++ app/src/main/nim/custom_nimview.nimble
@@ -11,58 +11,8 @@ license     = "MIT"
 
 # Currently, Webview requires gcc and doesn't work with vcc or clang
 
-requires "nim >= 0.17.0", "jester >= 0.5.0", "nimpy >= 0.1.1", "webview == 0.1.0"
-const uiDir = "../../nimview/examples/svelte"
+requires "nim >= 0.17.0", "jester >= 0.5.0", "nimpy >= 0.1.1", "webview == 0.1.0", "nake >= 1.9.0"
 const application = "custom_nimview"
 bin = @[application]
-const mainApp = application & ".nim"
-const libraryFile =  mainApp
 
-import oswalkdir, os, strutils  
-  
-let nimbleDir = parentDir(parentDir(system.findExe("nimble")))
-var nimbaseDir = parentDir(nimbleDir) & "/lib"
-if (not system.fileExists(nimbaseDir & "/nimbase.h")):
-  nimbaseDir = parentDir(parentDir(system.findExe("makelink"))) & "/lib"
-if (not system.fileExists(nimbaseDir & "/nimbase.h")):
-  nimbaseDir = parentDir(parentDir(parentDir(parentDir(system.findExe("gcc"))))) & "/lib"
-if (not system.fileExists(nimbaseDir & "/nimbase.h")):
-  nimbaseDir = parentDir(nimbleDir) & "/.choosenim/toolchains/nim-" & system.NimVersion & "/lib"
-cpFile(nimbaseDir / "nimbase.h", thisDir() / "../cpp" / "nimbase.h")
-  
-proc execCmd(command: string) = 
-  when defined(windows): 
-    exec "cmd /c \"" & command & "\""
-  else:
-    exec command
 
-proc buildCForArch(cpu, path: string) =
-  rmDir(path)
-  const stdOptions = "--header:" & application & ".h --app:staticlib -d:just_core -d:noSignalHandler -d:danger -d:release -d:androidNDK -d:noMain --os:android --threads:on "
-  selfExec " cpp -c " & stdOptions & "--cpu:" & cpu & " --nimcache:" & path & " " & mainApp
-
-proc buildC() = 
-  ## creates python and C/C++ libraries
-  buildCForArch("arm64", "./../cpp/arm64-v8a")
-  buildCForArch("arm", "./../cpp/armeabi-v7a")
-  buildCForArch("i386", "./../cpp/x86")
-  buildCForArch("amd64", "./../cpp/x86_64")
-
-proc buildJs() = 
-  # let oldDir = thisDir() 
-  # cd uiDir
-  # execCmd("npm install")
-  # cd oldDir
-  execCmd("npm run build --prefix " & uiDir)
-  # cpFile("../../nimview/src/backend-helper.js", uiDir & "/dist/backend-helper.js")
-  rmDir("../assets")
-  mkdir("../assets")
-  cpDir(uiDir & "/public", "../assets")
-  cpFile("../../nimview/src/backend-helper.js", uiDir & "/public/backend-helper.js")
-
-task buildCAndJs, "Create C files and compile svelte JS files":
-  buildC()
-  buildJs()
-
-task serve, "Serve NPM":
-  execCmd("npm run serve --prefix " & uiDir)
---
+++ app/src/main/nim/nakefile.nim
@@ -0,0 +1,64 @@
+import nake
+
+const application = "custom_nimview"
+const uiDir = "../../nimview/examples/svelte"
+const mainApp = application & ".nim"
+const libraryFile =  mainApp
+
+import os, strutils, system
+
+let nimbleDir = parentDir(parentDir(os.findExe("nimble")))
+var nimbaseDir = parentDir(nimbleDir) & "/lib"
+if (not os.fileExists(nimbaseDir & "/nimbase.h")):
+  nimbaseDir = parentDir(parentDir(os.findExe("makelink"))) & "/lib"
+if (not os.fileExists(nimbaseDir & "/nimbase.h")):
+  nimbaseDir = parentDir(parentDir(parentDir(parentDir(os.findExe("gcc"))))) & "/lib"
+if (not os.fileExists(nimbaseDir & "/nimbase.h")):
+  nimbaseDir = parentDir(nimbleDir) & "/.choosenim/toolchains/nim-" & system.NimVersion & "/lib"
+
+# echo "nimbaseDir: " & nimbaseDir
+
+proc execCmd(command: string) = 
+  echo "running: " & command
+  doAssert 0 == os.execShellCmd(command)
+
+proc buildCForArch(cpu, path: string) =
+  let cppPath = "../cpp" / path 
+  let headerFile = cppPath /  application & ".h"
+  if (headerfile.needsRefresh(mainApp)):
+    os.removeDir(cppPath)
+    const stdOptions = "--header:" & application & ".h --app:staticlib -d:just_core -d:noSignalHandler -d:release -d:androidNDK -d:noMain --os:android --threads:on "
+    execCmd(nimexe & " cpp -c " & stdOptions & "--cpu:" & cpu & " --nimcache:" & cppPath & " " & mainApp)
+
+proc buildC() =
+  ## creates python and C/C++ libraries
+  buildCForArch("arm64", "arm64-v8a")
+  buildCForArch("arm", "armeabi-v7a")
+  buildCForArch("i386", "x86")
+  buildCForArch("amd64", "x86_64")
+
+proc buildJs() =
+  var src: seq[string] = @[]
+  for path in walkDirRec(uiDir / "src"):
+    src.add(path)
+  if ((uiDir / "public/index.html").needsRefresh(src)):
+    # let oldDir = thisDir()
+    # cd uiDir
+    # execCmd("npm install")
+    # cd oldDir
+    execCmd("npm run build --prefix " & uiDir)
+    # cpFile("../../nimview/src/backend-helper.js", uiDir & "/dist/backend-helper.js")
+    os.removeDir("../assets")
+    os.createDir("../assets")
+    os.copyDir(uiDir & "/public", "../assets")
+    os.copyFile(system.currentSourcePath().parentDir() / "../../nimview/src/backend-helper.js", "../assets/backend-helper.js")
+
+task "serve", "Serve NPM":
+  doAssert 0 == os.execShellCmd("npm run serve --prefix " & uiDir)
+
+
+
+task defaultTask, "Compiles to C":
+  buildC()
+  buildJs()
+  os.copyFile(nimbaseDir / "nimbase.h", system.currentSourcePath().parentDir() / "../cpp" / "nimbase.h")
