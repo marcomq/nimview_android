36a3558d7080828aa17266f7984b48c5550a16d2
Marco Mengelkoch <Mmengelkoch@gmx.de>
2021-03-17 11:19:42 +0100

    improved hpp usage for cpp libraries, simplified native-lib.cpp call

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,7 @@
 #include <string>
 #include <string.h>
 #include "custom_nimview.h"
-
-
-thread_local bool nimInitialized = false;
+#include "nimview.hpp"
 
 #define THIS_PROJECT_PREFIX Java_com_nimviewAndroid
 extern "C" JNIEXPORT jstring JNICALL
@@ -13,11 +11,7 @@ Java_com_nimviewAndroid_NativeCpp_callNim(
         jobject /* this */, jstring request, jstring value) {
     char* cRequest = const_cast<char*>(env->GetStringUTFChars(request, nullptr));
     char* cValue = const_cast<char*>(env->GetStringUTFChars(value, nullptr));
-    if (!nimInitialized) {
-        NimMain();
-        nimInitialized = true;
-    }
-    std::string result(nimview_dispatchRequest(cRequest, cValue));
+    std::string result(nimview::dispatchRequest(cRequest, cValue));
     env->ReleaseStringUTFChars(request, cRequest);
     env->ReleaseStringUTFChars(value, cValue);
     return env->NewStringUTF(result.c_str());
